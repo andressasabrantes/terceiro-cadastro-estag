9a3cf125e32997cddaabb5f4cb356bfadfe93031
Lucas Alves <lucasanc14@gmail.com>
2022-11-01 09:02:59 -0300

    Revert "Lucas: adicionando meu apelido e meu cargo"
    This reverts commit 2e4f74d214b304b2d4a80b5dd7e6573ecdf2e81b.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$(".luqinhas").on("click", function() {
-    $(".estagiario1").show();
+$(".apelido").on("click", function() {
+    $(".cargo").show();
+})
+$(".amaldi").on("click", function() {
+    $(".estag_1").show();
+})
+$(".lica").on("click", function() {
+    $(".estagiario").show();
+})
+$(".gemeas").on("click", function() {
+    $(".desenvolvedora").show();
+})
+$(".betoneira").on("click", function() {
+    $(".dev").show();
+})
+$(".gema").on("click", function() {
+    $(".softwareeng").show();
 })
