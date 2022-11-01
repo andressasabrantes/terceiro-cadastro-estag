35661d328bb95d43a58f7f78bb77145ddfbb2e7f
Andressa Abrantes <andressasabrantes@gmail.com>
2022-11-01 00:18:58 -0300

    andressa: trazendo a branch master pra minha e resolvendo os conflitos

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-$(".gemeas").on("click", function() {
-    $(".desenvolvedora").show();
+$(".apelido").on("click", function() {
+    $(".cargo").show();
 })
 $(".amaldi").on("click", function() {
     $(".estag_1").show();
 })
 $(".lica").on("click", function() {
     $(".estagiario").show();
+})
+$(".gemeas").on("click", function() {
+    $(".desenvolvedora").show();
 })
