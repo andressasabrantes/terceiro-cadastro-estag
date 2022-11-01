08ff601eb841a18b0e5442491920a24dc3ef0685
Lívia Rocha <liv.rocha@outlook.com>
2022-11-01 00:04:49 -0300

    livia: inserção dos meus dados no html, js e css

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-$(".lica").on("click", function() {
-    $(".estagiario").show();
+$(".apelido").on("click", function() {
+    $(".cargo").show();
 })
 $(".amaldi").on("click", function() {
     $(".estag_1").show();
+})
+$(".lica").on("click", function() {
+    $(".estagiario").show();
 })
